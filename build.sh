c1897776c681f83024c8ac1a26986f1b430f9e3f
Nguyen Marc <nguyen_marc@live.fr>
2024-07-01 00:21:51 +0200

    New Buildscript

--- build.sh
+++ build.sh
@@ -1,17 +1,27 @@
 #!/bin/sh
 
-mkdir -p out
+set -ex
+
+mkdir -p out/
 
 # Build amd64 kernel
 podman run --rm -it \
   -v "$(pwd):/work" \
+  -e ARCH=amd64 \
   ghcr.io/darkness4/k3os-kernel:latest-base
+
+mv dist/artifacts/* out/
+
+./scripts/clean
 
 # Build arm64 kernel
 podman run --rm -it \
   -v "$(pwd):/work" \
+  -e ARCH=arm64 \
   ghcr.io/darkness4/k3os-kernel:latest-base sh -c 'export $(dpkg-architecture -aarm64)
 export CROSS_COMPILE=aarch64-linux-gnu-
 
-bash
+./scripts/ci
 '
+
+mv dist/artifacts/* out/
